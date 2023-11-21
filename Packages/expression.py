1f0a15040cdb18fa971c12c57045c9b17c2d90e2
tikhon <tikhon.radk@gmail.com>
2023-11-21 15:00:27 +0300

    error handling added

--- Packages/expression.py
+++ Packages/expression.py
@@ -12,12 +12,15 @@ class Expression:
                 self.args == other.args and self.result == other.result)
 
     def calculate(self):
-        converted_expression = self.expression
-        for key, value in self.args.items():
-            converted_expression = converted_expression.replace(str(key), str(value))
+        try:
+            converted_expression = self.expression
+            for key, value in self.args.items():
+                converted_expression = converted_expression.replace(str(key), str(value))
 
-        self.converted_expression = converted_expression
-        self.result = eval(self.converted_expression)
+            self.converted_expression = converted_expression
+            self.result = eval(self.converted_expression)
+        except Exception as e:
+            raise ValueError(f"Error calculating the \n{self.expression}: \n{e}")
 
     def get_description(self):
         description = f"{'#'*15} {self.title} {'#'*15}\n\n"
