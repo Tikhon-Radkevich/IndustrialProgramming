1d09737b7078bd5a660f7ef24a264dcda7ad9719
tikhon <tikhon.radk@gmail.com>
2023-12-19 14:14:08 +0300

    CONTENT_TYPES added

--- config.py
+++ config.py
@@ -4,3 +4,9 @@ import os
 CACHE_DIR = ".cache"
 EXAMPLE_FILES_PATH = "data/examples"
 WORKING_PATH = "data/working"
+
+CONTENT_TYPES = {
+    ".json": "application/json",
+    ".xml": "application/xml",
+    ".zip": "application/zip"
+}
